cf7867866d4957025120af7ded418c11b884c6e0
javier <javier.aguayo19@gmail.com>
2021-05-12 04:49:35 -0400

    llamando a un componente tabla dentro del componente cliente

--- src/views/Cliente.vue
+++ src/views/Cliente.vue
@@ -1,7 +1,22 @@
 <template>
   <v-container>
-    <h1>This is an Cliente page</h1>
+    <h1>pagina de clientes</h1>
+   <Table/>
   </v-container>
-    
-  
 </template>
+
+<script>
+
+  import Table from '@/components/Table';
+
+  export default {
+    name: 'Cliente',
+    components : {
+     Table
+    },
+
+    data: () => ({
+    
+    }),
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,6 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex class="primary" xs12>
-        <h1>12</h1>
-      </v-flex>
-
-      <v-flex class="info" xs12>
-        <h1>6</h1>
-      </v-flex>
-    </v-layout>
+    <h1>This is an Home page</h1>
   </v-container>
     
   
