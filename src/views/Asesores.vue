81db70326a1541075e76abb82d0e389bb2351e5d
javier <javier.aguayo19@gmail.com>
2021-05-12 03:32:17 -0400

    rutas , cliente, asesores y inversionistas

---
+++ src/views/Asesores.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container>
+    <h1>This is an Asesores page</h1>
+  </v-container>
+    
+  
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-layout>
+      <v-flex class="primary" xs12>
+        <h1>12</h1>
+      </v-flex>
+
+      <v-flex class="info" xs12>
+        <h1>6</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+    
+  
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
