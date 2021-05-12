db80129f3113ee4c1352d1912f5a4f320b6c7083
javier <javier.aguayo19@gmail.com>
2021-05-11 23:15:27 -0400

    agregado vue route

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,11 @@
 
       <v-spacer></v-spacer>
 
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
@@ -36,25 +41,47 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
+    
 
     <v-main>
-      <HelloWorld/>
+        <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+/* import HelloWorld from './components/HelloWorld'; */
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    
   },
-
   data: () => ({
     //
   }),
 };
 </script>
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
